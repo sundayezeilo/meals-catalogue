2ed70b7bfd91bea03bcf7bea8e99a9b35692b4a8
ezeilo-su <ezeilo.su@gmail.com>
2021-04-18 09:16:38 +0100

    Style footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import './Footer.css';
 
 const Footer = () => (
@@ -7,22 +10,31 @@ const Footer = () => (
 
       <div className="footer-wrap row">
 
-        <div className="col-4 footer-brand">
-          <a href="/" className="page-brand">MealsCatalogue</a>
+        <div className="col-3 footer-brand-wrap">
+          <a href="/" className="footer-brand">MealsCatalogue</a>
         </div>
 
-        <div className="col-4 copy d-md-flex justify-content-center">
-          <div className="privacy-text">Privacy policy Terms &amp; Conditions</div>
-          <div> &copy; Copyright 2021.  All rights reserved.</div>
-        </div>
+        <div className="col-3 d-md-flex justify-content-center privacy-text">Privacy policy Terms &amp; Conditions</div>
 
-        <div className="col-4 social-wrap">
-          <ul className="social-network">
-            <li className="social-circle"><a href="https://github.com/ezeilo-su" className="icoGitHub" target="_blank" rel="noreferrer"><i className="fa fa-github social-icons" aria-hidden="true" /></a></li>
-            <li className="social-circle"><a href="https://www.linkedin.com/in/sundayezeilo/" className="icoLinkedin" target="_blank" rel="noreferrer"><i className="fa fa-linkedin social-icons" aria-hidden="true" /></a></li>
-            <li className="social-circle"><a href="https://twitter.com/SundayEzeilo" className="icoTwitter" target="_blank" rel="noreferrer"><i className="fa fa-twitter social-icons" aria-hidden="true" /></a></li>
-          </ul>
-        </div>
+        <div className="col-3 d-md-flex justify-content-center legal"> &copy; Copyright 2021.  All rights reserved.</div>
+
+        <ul className="col-2 social-network">
+          <li className="social-circle">
+            <a href="https://github.com/ezeilo-su" className="icoGitHub" target="_blank" rel="noreferrer">
+              <GitHubIcon />
+            </a>
+          </li>
+          <li className="social-circle">
+            <a href="https://www.linkedin.com/in/sundayezeilo/" className="icoLinkedin" target="_blank" rel="noreferrer">
+              <LinkedInIcon />
+            </a>
+          </li>
+          <li className="social-circle">
+            <a href="https://twitter.com/SundayEzeilo" className="icoTwitter" target="_blank" rel="noreferrer">
+              <TwitterIcon />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </footer>
