be8db88f9cce69466a676664f80de163db57347a
ezeilo-su <ezeilo.su@gmail.com>
2021-04-20 22:03:35 +0100

    Discard redundant links in the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,30 +5,15 @@ import logo from '../../images/logo.jpg';
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link to="/" className="nav-brand">
+    <nav id="navbar" className="d-flex align-items-center justify-content-between">
+      <Link to="/">
+        <div className="nav-brand">
           <img src={logo} alt="brand" />
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/" className="nav-link nav-home active" aria-current="page">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Services</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Contact</Link>
-            </li>
-          </ul>
         </div>
+        <h2 className="brand-text">Meals Catalogue</h2>
+      </Link>
+      <div className="nav-item">
+        <Link to="/" className="nav-link nav-home active" aria-current="page">Home</Link>
       </div>
     </nav>
   );
